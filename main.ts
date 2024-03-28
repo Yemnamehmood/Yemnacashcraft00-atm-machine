9f9a5b0e07308dfc69d7eb761f12a14c44140f45
YemnaMehmood <163342865+Yemnamehmood@users.noreply.github.com>
2024-03-28 06:48:39 +0500

    My project cashcraft00-atm-machine
    This is simple atm machine and pin code is 1334.

---
+++ main.ts
@@ -0,0 +1,90 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let mybalance = 30000; //dollar
+let mypin = 1334; 
+
+let pinAnswer = await inquirer.prompt(
+    [
+        {
+            name: "pin",
+            message: "Enter you pin code",
+            type: "number"
+        }
+    ]
+);
+
+if (pinAnswer.pin === mypin){
+    console.log("Correct Pin Code!!");
+
+ let operationAns = await inquirer.prompt(
+        [
+            {
+                name: "operation",
+                message: "please select an option",
+                type: "list",
+                choices: ["Withdraw", "Check Balance", "Fast Cash","Exit"]
+            }
+        ]
+    );
+    console.log(operationAns);
+
+    if(operationAns.operation==="Withdraw"){
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "Amount",
+                    message: "Enter the amount to withdraw",
+                    type: "number"
+                }
+            ]
+        );
+
+        console.log(amountAns.Amount);
+
+        mybalance -= amountAns.Amount;
+        console.log(`Your remaining balance is: ${mybalance}`);
+
+    } else if(operationAns.operation==="Check Balance"){
+        console.log(`Your Current Balance Amount is:${mybalance}`);
+
+    } else if (operationAns.operation ==="Fast Cash"){
+        let fastcashAns = await inquirer.prompt(
+            [
+                {
+                    name: "option",
+                    message: "Select Fast Cash Amount",
+                    type: "list",
+                    choices: ["1000","2000","4000","6000","7000","10000"],
+                }
+            ]
+        );
+        mybalance -= fastcashAns.option;
+        console.log(`Your remaining Current Amount is:  ${mybalance}`);
+
+
+        
+    } 
+     else if (operationAns.operation === "Exit") {
+        console.log("Thank you for using the ATM. Goodbye!");
+
+     }
+
+
+
+}
+
+
+else {
+    console.log("Incorrect Pin Number");
+}
+
+
+
+
+
+
+
+//do fast cash h.w 4 options 1000,2000,5000,10,000
+// template literals use coordination
+//insufficient balance h.w
